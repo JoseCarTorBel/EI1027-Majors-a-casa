0697712d4fe73f43bd624f0ae71b1cfcd240be10
iamximo <al375824@uji.es>
2020-05-09 14:46:19 +0200

    Añadido numero a las direcciones

--- GESTION_BDD/all_inserts.sql
+++ GESTION_BDD/all_inserts.sql
@@ -4,23 +4,23 @@ INSERT INTO socialworker VALUES ('20919929E','SW_Carlos Rodriguez','766542432', 
 INSERT INTO socialworker VALUES ('20099920W','SW_Laura Torro','644333123', 'SW_LauraTorro', 'SW_LauraTorro');
 INSERT INTO socialworker VALUES ('19029093W','SW_Marta Lacueva','666291873', 'SW_MartaLacueva', 'SW_MartaLacueva');
 
-INSERT INTO person VALUES ('V_Pablo','Sanahuja Leira','20904567S','675678765','1999-02-09','Ronda Vinatea, Castellon de la plana, Castellon','al375833@uji.es','v_pablo', 'v_pablo');
-INSERT INTO person VALUES ('V_Juan','Gonzalez Lopez','20764726T','679564357','1998-01-09','Calle sagrada familia, Valencia, Valencia','juanito@yahoo.es','v_juan', 'v_juan');
-INSERT INTO person VALUES ('V_Jaime','Ibañez Torro','23457855W','692458891','2000-09-09','Avenida Vinatea, Alcora, Castellon','jaimito22@yahoo.es','v_jaime', 'v_jaime');
-INSERT INTO person VALUES ('V_Pepe','Reina Beltran','20998812E','622198744','1997-11-09','Plaza la paz, Castellon de la plana, Castellon','pepereina@uji.es','v_pepe', 'v_pepe');
-INSERT INTO person VALUES ('V_Laura','Martinez Palotes','20912333Y','623317367','2000-11-09','Plaza la lola, Alcora, Castellon','laurita@uji.es','v_laura', 'v_laura');
-INSERT INTO person VALUES ('V_Marcos','Kinto Trujillo','20998877E','655442123','2010-10-16','Plaza Juan, Valencia, Valencia','marquitosss@uji.es','v_marcos', 'v_marcos');
-INSERT INTO person VALUES ('V_Lucia','De la cueva Mijatres','20112233Y','655223991','2001-11-20','Plaza Jaime I, Vinaroz, Castellon','luciadelacu@uji.es','v_lucia', 'v_lucia');
+INSERT INTO person VALUES ('V_Pablo','Sanahuja Leira','20904567S','675678765','1999-02-09','Ronda Vinatea 23 1B, Castellon de la plana, Castellon','al375833@uji.es','v_pablo', 'v_pablo');
+INSERT INTO person VALUES ('V_Juan','Gonzalez Lopez','20764726T','679564357','1998-01-09','Calle sagrada familia S/N , Valencia, Valencia','juanito@yahoo.es','v_juan', 'v_juan');
+INSERT INTO person VALUES ('V_Jaime','Ibañez Torro','23457855W','692458891','2000-09-09','Avenida Vinatea, Alcora 1 PB, Castellon','jaimito22@yahoo.es','v_jaime', 'v_jaime');
+INSERT INTO person VALUES ('V_Pepe','Reina Beltran','20998812E','622198744','1997-11-09','Plaza la paz numero8 piso2 puerta6, Castellon de la plana, Castellon','pepereina@uji.es','v_pepe', 'v_pepe');
+INSERT INTO person VALUES ('V_Laura','Martinez Palotes','20912333Y','623317367','2000-11-09','Plaza la lola 9 1 P2, Alcora, Castellon','laurita@uji.es','v_laura', 'v_laura');
+INSERT INTO person VALUES ('V_Marcos','Kinto Trujillo','20998877E','655442123','2010-10-16','Plaza Juan 40 6B, Valencia, Valencia','marquitosss@uji.es','v_marcos', 'v_marcos');
+INSERT INTO person VALUES ('V_Lucia','De la cueva Mijatres','20112233Y','655223991','2001-11-20','Plaza Jaime I 2 17U, Vinaroz, Castellon','luciadelacu@uji.es','v_lucia', 'v_lucia');
 
 
-INSERT INTO person VALUES ('EP_Jose Carlos','Torro Belda','20654491U','672945673','1950-02-04','Ronda Vinatea, Ontinyete, Alicante','josecarlos@uji.es','EP_Jose_Carlos', 'EP_Jose_Carlos');
-INSERT INTO person VALUES ('EP_Joaquin','Gonzalez Alvarez','20884391O','627367564','1940-01-03','Calle pepe, Ontinyete, Alicante','joaquin@uji.es','EP_Joaquin', 'EP_Joaquin');
-INSERT INTO person VALUES ('EP_Maria','Peris Alonso','20119392I','612737283','1935-06-01','Calle egipcia, Ontinyete, Alicante','maria23@uji.es','EP_Maria', 'EP_Maria');
-INSERT INTO person VALUES ('EP_Marta','Lopez Marza','20119290R','675672934','1937-11-20','Avenida casalduch, Castellon de la plana, Castellon','marta@uji.es','EP_Marta', 'EP_Marta');
-INSERT INTO person VALUES ('EP_Cristina','Sanahuja Leira','20917888Y','624378234','1953-10-26','Calle la calle, Ontinyete, Alicante',null,'EP_Cristina', 'EP_Cristina');
-INSERT INTO person VALUES ('EP_Cristina','Sanahuja Leira','20917888Y','624378234','1953-10-26','Calle la calle, Ontinyete, Alicante',null,'EP_Cristina', 'EP_Cristina');
-INSERT INTO person VALUES ('EP_Juanlu','Perez Llanos ','34758384E','675432345','1953-10-26','Calle la calle, Valencia, Valencia',null,'EP_Juanlu', 'EP_Juanlu');
-INSERT INTO person VALUES ('EP_Ibai','Etxebarrieta Ybarra ','34567654R','675432345','1953-10-26','Calle la calle, Vila-Real, Castellon',null,'EP_Ibai', 'EP_Ibai');
+INSERT INTO person VALUES ('EP_Jose Carlos','Torro Belda','20654491U','672945673','1950-02-04','Ronda Vinatea 56 1B, Ontinyete, Alicante','josecarlos@uji.es','EP_Jose_Carlos', 'EP_Jose_Carlos');
+INSERT INTO person VALUES ('EP_Joaquin','Gonzalez Alvarez','20884391O','627367564','1940-01-03','Calle pepe 1 5B, Ontinyete, Alicante','joaquin@uji.es','EP_Joaquin', 'EP_Joaquin');
+INSERT INTO person VALUES ('EP_Maria','Peris Alonso','20119392I','612737283','1935-06-01','Calle egipcia 4, Ontinyete, Alicante','maria23@uji.es','EP_Maria', 'EP_Maria');
+INSERT INTO person VALUES ('EP_Marta','Lopez Marza','20119290R','675672934','1937-11-20','Avenida casalduch 1A Castellon de la plana, Castellon','marta@uji.es','EP_Marta', 'EP_Marta');
+INSERT INTO person VALUES ('EP_Cristina','Sanahuja Leira','20917888Y','624378234','1953-10-26','Calle la calle S/N, Ontinyete, Alicante',null,'EP_Cristina', 'EP_Cristina');
+INSERT INTO person VALUES ('EP_Cristina','Sanahuja Leira','20917888Y','624378234','1953-10-26','Calle la calle 9 3p4, Ontinyete, Alicante',null,'EP_Cristina', 'EP_Cristina');
+INSERT INTO person VALUES ('EP_Juanlu','Perez Llanos ','34758384E','675432345','1953-10-26','Calle la calle 9 3p3, Valencia, Valencia',null,'EP_Juanlu', 'EP_Juanlu');
+INSERT INTO person VALUES ('EP_Ibai','Etxebarrieta Ybarra ','34567654R','675432345','1953-10-26','Calle la calle 8 2A, Vila-Real, Castellon',null,'EP_Ibai', 'EP_Ibai');
 
 INSERT INTO volunteer VALUES ('20904567S','2023-01-01','A');
 INSERT INTO volunteer VALUES ('20764726T','2021-01-01','R');
@@ -61,11 +61,11 @@ INSERT INTO disponibility VALUES ('20112233Y',null,1,'2019-01-01','2020-12-01','
 INSERT INTO disponibility VALUES ('20998877E',null,3,'2020-01-01','2020-12-01','P');
 INSERT INTO disponibility VALUES ('20112233Y',null,2,'2019-01-01','2020-12-01','P');
 
-INSERT INTO company VALUES ('G34672398','Catering Miravete','12345678G','Vicente Miravete','647352635','vmiravete@gmail.com','Calle Catalunya, Castelló de la Plana, Castellón',null,null,NOW());
-INSERT INTO company VALUES ('T46372819','Dra González','45632178F','Sandra González','657489325','sandragonzalez@gmail.com','Calle Rafalafena, Castelló de la Plana, Castellon',null,null,NOW());
-INSERT INTO company VALUES ('R36271856','Limpiezas Pedro','14253698A','Pedro Alonso','678543234','pedroalonso@gmail.com','Calle Mozart, Castelló de la Plana, Castellon','C_Limiezas','C_Limiezas',NOW());
-INSERT INTO company VALUES ('U68352416','Conchi Limpiezas','25361482A','Concepción García','687935268','conchigarcia@gmail.com','Calle de Arriba, Castelló de la Plana, Castellon','C_Irco','C_Irco',NOW());
-INSERT INTO company VALUES ('L84736273','Irco','689543765','María del Mar Álvarez','14578965A','maralvarez@irco.es','Gran Vía, Castelló de la Plana, Castellon',null,null,NOW());
+INSERT INTO company VALUES ('G34672398','Catering Miravete','12345678G','Vicente Miravete','647352635','vmiravete@gmail.com','Calle Catalunya 3, Castelló de la Plana, Castellón',null,null,NOW());
+INSERT INTO company VALUES ('T46372819','Dra González','45632178F','Sandra González','657489325','sandragonzalez@gmail.com','Calle Rafalafena 4, Castelló de la Plana, Castellon',null,null,NOW());
+INSERT INTO company VALUES ('R36271856','Limpiezas Pedro','14253698A','Pedro Alonso','678543234','pedroalonso@gmail.com','Calle Mozart 1 1B, Castelló de la Plana, Castellon','C_Limiezas','C_Limiezas',NOW());
+INSERT INTO company VALUES ('U68352416','Conchi Limpiezas','25361482A','Concepción García','687935268','conchigarcia@gmail.com','Calle de Arriba 89 P3, Castelló de la Plana, Castellon','C_Irco','C_Irco',NOW());
+INSERT INTO company VALUES ('L84736273','Irco','689543765','María del Mar Álvarez','14578965A','maralvarez@irco.es','Gran Vía 2 P1, Castelló de la Plana, Castellon',null,null,NOW());
 
 
 INSERT INTO contract VALUES ('020200101G34672398','G34672398',0,'2020-01-01','2021-01-01',130,'Dilluns;Dimarts;Dimecres;Dijous;Divendres',CAST('12:00' AS TIME), CAST('14:30' AS TIME));
